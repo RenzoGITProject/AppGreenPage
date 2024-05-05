ff88fd488b1935bb63170a2a4c1721a70239d2a0
RenzoGITProject <quelopana.pantigoso.renzo@gmail.com>
2024-05-05 01:26:39 -0500

    Creacion del Layout ListaSubastas

---
+++ app/src/main/java/com/alonsoms/appt1alonsoms/entities/clasemodelos.kt
@@ -0,0 +1,56 @@
+package com.alonsoms.appt1alonsoms.entities
+
+class clasemodelos {
+    class Usuario {
+        lateinit var nombres: String
+        lateinit var correo: String
+        lateinit var username: String
+        lateinit var contrasena: String
+    }
+
+    class PerfilUsuario {
+        lateinit var nombrePerfil: String
+    }
+
+    class Editorial {
+        lateinit var nombre: String
+    }
+
+    class Genero {
+        lateinit var nombre: String
+    }
+
+    class Libro {
+        lateinit var titulo: String
+        var estado: String? = null
+        lateinit var isbn: String
+        var idEditorial: Long = 0
+        var idGenero: Long = 0
+        lateinit var sinopsis: String
+    }
+
+    class Subasta {
+        var idLibro: Long = 0
+        lateinit var fechaInicio: String
+        lateinit var fechaFinal: String
+        var precioBase: Float = 0f
+    }
+
+    class Calificacion {
+        var puntaje: Int = 0
+        lateinit var comentario: String
+        var idLibro: Long = 0
+    }
+
+    class Oferta {
+        var idSubasta: Long = 0
+        var precioOferta: Float = 0f
+    }
+
+    class Resena {
+        var idLibro: Long = 0
+        var idUsuario: Long = 0
+    }
+
+
+}
