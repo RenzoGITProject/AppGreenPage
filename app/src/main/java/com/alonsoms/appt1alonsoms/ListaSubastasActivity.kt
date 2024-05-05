d57912d60efc3985d9082de4e812af7a6619c3c3
MARIO <mayalat260@gmail.com>
2024-05-04 22:38:22 -0500

    Se brinda referencia al boton Ofertar para que pueda abrir el listarOfertas.

--- app/src/main/java/com/alonsoms/appt1alonsoms/ListaSubastasActivity.kt
+++ app/src/main/java/com/alonsoms/appt1alonsoms/ListaSubastasActivity.kt
@@ -1,20 +1,28 @@
 package com.alonsoms.appt1alonsoms
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListaSubastasActivity : AppCompatActivity() {
+
+    private lateinit var btnOfertar:FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_lista_subastas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        asignarReferencias()
+    }
+    fun asignarReferencias(){
+        btnOfertar = findViewById(R.id.btnOfertar)
+        btnOfertar.setOnClickListener {
+            val intent = Intent(this,ListarOfertasActivity::class.java)
+            startActivity(intent)
         }
     }
 }
