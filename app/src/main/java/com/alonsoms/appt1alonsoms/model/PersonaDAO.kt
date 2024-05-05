3ffcc6936c923a703cec9583ff2d1e8a904c4f11
MARIO <mayalat260@gmail.com>
2024-05-05 01:18:50 -0500

    Se brinda referencia al boton Ofertar para que pueda abrir el listarOfertas.

--- app/src/main/java/com/alonsoms/appt1alonsoms/model/PersonaDAO.kt
+++ app/src/main/java/com/alonsoms/appt1alonsoms/model/PersonaDAO.kt
@@ -10,14 +10,20 @@ import com.alonsoms.appt1alonsoms.util.SQLiteHelper
 class PersonaDAO(context: Context)  {
     private var sqLiteHelper: SQLiteHelper = SQLiteHelper(context)
 
-    fun cargarSubasta():ArrayList<Persona>{
+    fun cargarOferta():ArrayList<Persona>{
         val listaPersona:ArrayList<Persona> = ArrayList()
         val query = "SELECT * FROM ofertas"
         val db = sqLiteHelper.readableDatabase
         val cursor:Cursor
 
         try {
+            cursor = db.rawQuery(query,null)
+            do {
+                val IdOferta:Int = cursor.getInt(cursor.getColumnIndexOrThrow("IdOferta"))
+                val IdSubasta:Int =cursor.getInt(cursor.getColumnIndexOrThrow("IdSubasta"))
+                val PrecioOferta:Float = cursor.getFloat(cursor.getColumnIndexOrThrow("IdSubasta"))
 
+            }while (cursor.moveToNext())
         }catch (e:Exception){
             Log.d("===", e.message.toString())
 
