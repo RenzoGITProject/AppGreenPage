107f7573038554752d3d7583191febc842d2103f
RenzoGITProject <quelopana.pantigoso.renzo@gmail.com>
2024-05-05 00:31:04 -0500

    Creacion del Layout ListaSubastas

--- app/src/main/java/com/alonsoms/appt1alonsoms/util/SQLiteHelper.kt
+++ app/src/main/java/com/alonsoms/appt1alonsoms/util/SQLiteHelper.kt
@@ -13,15 +13,77 @@ class SQLiteHelper(context: Context) :SQLiteOpenHelper(context,DATABASE_NAME,nul
    }
 
     /* Se crea la base de datos*/
-    override fun onCreate(db: SQLiteDatabase) {
-        val sql=    "CREATE TABLE IF NOT EXISTS persona " +
-                    "(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    " dni INTEGER NOT NULL, " +
-                    " nombres TEXT NOT NULL, " +
-                    " correo TEXT NOT NULL );"
-        db.execSQL(sql)
+        override fun onCreate(db: SQLiteDatabase) {
+            val sqlUsuario = "CREATE TABLE IF NOT EXISTS usuario " +
+                    "(IdUsuario INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Nombres VARCHAR(255) NOT NULL, " +
+                    "Login VARCHAR(255) NOT NULL, " +
+                    "Password VARCHAR(255) NOT NULL, " +
+                    "FechaCreacion DATETIME DEFAULT CURRENT_TIMESTAMP);"
+            db.execSQL(sqlUsuario)
 
-    }
+            val sqlPerfilUsuario = "CREATE TABLE IF NOT EXISTS perfilUsuario " +
+                    "(IdPerfilUsuario INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "NombrePerfil VARCHAR(255) NOT NULL);"
+            db.execSQL(sqlPerfilUsuario)
+
+            val sqlEditorial = "CREATE TABLE IF NOT EXISTS editorial " +
+                    "(IdEditorial INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Nombre VARCHAR(255) NOT NULL);"
+            db.execSQL(sqlEditorial)
+
+            val sqlGenero = "CREATE TABLE IF NOT EXISTS genero " +
+                    "(IdGenero INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Nombre VARCHAR(255) NOT NULL);"
+            db.execSQL(sqlGenero)
+
+            val sqlLibros = "CREATE TABLE IF NOT EXISTS libros " +
+                    "(IdLibro INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Titulo VARCHAR(255) NOT NULL, " +
+                    "Estado VARCHAR(255), " +
+                    "ISBN VARCHAR(255) UNIQUE NOT NULL, " +
+                    "IdEditorial INTEGER, " +
+                    "IdGenero INTEGER, " +
+                    "Sinopsis VARCHAR(255), " +
+                    "FOREIGN KEY (IdEditorial) REFERENCES editorial(IdEditorial), " +
+                    "FOREIGN KEY (IdGenero) REFERENCES genero(IdGenero));"
+            db.execSQL(sqlLibros)
+
+            val sqlSubasta = "CREATE TABLE IF NOT EXISTS subasta " +
+                    "(IdSubasta INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "IdLibro INTEGER, " +
+                    "FechaInicio DATETIME, " +
+                    "FechaFinal DATETIME, " +
+                    "PrecioBase FLOAT, " +
+                    "FOREIGN KEY (IdLibro) REFERENCES libros(IdLibro));"
+            db.execSQL(sqlSubasta)
+
+            val sqlCalificacion = "CREATE TABLE IF NOT EXISTS calificacion " +
+                    "(IdCalificacion INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "Puntaje INTEGER, " +
+                    "Comentario VARCHAR(255), " +
+                    "IdLibro INTEGER, " +
+                    "FOREIGN KEY (IdLibro) REFERENCES libros(IdLibro));"
+            db.execSQL(sqlCalificacion)
+
+            val sqlOfertas = "CREATE TABLE IF NOT EXISTS ofertas " +
+                    "(IdOferta INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "IdSubasta INTEGER, " +
+                    "PrecioOferta FLOAT, " +
+                    "FOREIGN KEY (IdSubasta) REFERENCES subasta(IdSubasta));"
+            db.execSQL(sqlOfertas)
+
+            val sqlResena = "CREATE TABLE IF NOT EXISTS resena " +
+                    "(IdResena INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "IdLibro INTEGER, " +
+                    "IdUsuario INTEGER, " +
+                    "FOREIGN KEY (IdLibro) REFERENCES libros(IdLibro), " +
+                    "FOREIGN KEY (IdUsuario) REFERENCES usuario(IdUsuario));"
+            db.execSQL(sqlResena)
+        }
+
+
+
 
     /* Se ejecuta una sola vez*/
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
